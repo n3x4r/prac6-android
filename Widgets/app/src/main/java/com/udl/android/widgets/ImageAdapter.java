6361496c7a25066816e0ca24bcc3d5db04e642f4
Sergio Cacho <sergi0.cach0@hotmail.com>
2017-03-28 00:08:49 +0200

    ok

--- Widgets/app/src/main/java/com/udl/android/widgets/ImageAdapter.java
+++ Widgets/app/src/main/java/com/udl/android/widgets/ImageAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 
@@ -14,13 +15,15 @@ import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private int [] images;
 
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c, int[] images) {//se pasa la activity(Layout) y el array de imagenes
         mContext = c;
+        this.images = images;
     }
 
     public int getCount() {
-        return mThumbIds.length;
+        return images.length;
     }
 
     public Object getItem(int position) {
@@ -32,24 +35,21 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) { // se debe de hacer con adapters para crear botones
+    public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setLayoutParams(new GridView.LayoutParams(400, 400));
+            imageView.setScaleType(ImageView.ScaleType.FIT_END);
             imageView.setPadding(8, 8, 8, 8);
+
         } else {
             imageView = (ImageView) convertView;
         }
 
-        imageView.setImageResource(mThumbIds[position]);
+        imageView.setImageResource(this.images[position]);
         return imageView;
     }
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.android_focused, R.drawable.android_normal,
-    };
 }
--- Widgets/app/src/main/java/com/udl/android/widgets/list_grid_view.java
+++ Widgets/app/src/main/java/com/udl/android/widgets/list_grid_view.java
@@ -2,6 +2,7 @@ package com.udl.android.widgets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,21 +22,19 @@ public class list_grid_view extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_grid_view);
         GridView gridv = (GridView)findViewById(R.id.gridview);
-        String[] vendorArray = { "RIM", "Palm", "Nokia", "Iphone", "Mac" , "Android" };
-        List<String> lista = new ArrayList<String>(Arrays.asList(vendorArray));
-        Collections.shuffle(lista);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, lista);
-        gridv.setAdapter(adapter);
+        final String[] vendorArray = getResources().getStringArray(R.array.grid_view);
+        int[] images ={R.drawable.dell,R.drawable.hp, R.drawable.samsung, R.drawable.sonyericsson, R.drawable.motorola, R.drawable.huawei, R.drawable.tmobile, R.drawable.htc};
+        gridv.setAdapter(new ImageAdapter(this, images)); //existe una clase adapter que hereda de Base adapter que se encarga de preparar la gridview con los elementos
+        gridv.setGravity(Gravity.CENTER);
         gridv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String)  parent.getItemAtPosition(position);
-
-                String message = String.format(getString(R.string.plantilla_mensaje_spinner), item);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {//optengo la posicion de la imagen y la asocio con su nombre
+                String message = String.format(getString(R.string.plantilla_mensaje_spinner), vendorArray[position]);//aqui uso el array de Fabricantes = vendorArray para optener el nombre del fabricante
                 Toast.makeText(list_grid_view.this, message , Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 }
